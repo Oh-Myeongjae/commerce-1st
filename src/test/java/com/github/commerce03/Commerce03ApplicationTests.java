bbe5bf5621d41daf039bf26c45072cb882572d13
jin1220 <h6779@naver.com>
2023-08-08 18:12:56 +0900

    20230808

---
+++ src/test/java/com/github/commerce03/Commerce03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.commerce03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Commerce03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
